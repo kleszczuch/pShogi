5eb57c7b1155eaf063a4b8ba3589f94c3a42a8db
kleszczu <marek.kleszcz2004@gmail.com>
2025-01-03 21:23:51 +0100

    visual board is ready, renamed images folder and added background png

---
+++ PiecesMoves/Pawn.py
@@ -0,0 +1,11 @@
+class pawn():
+    def __init__(self, color):
+        self.color = color
+         
+        
+        
+    def move(self):
+        if self.color == 'white':
+            if 
+
+    
